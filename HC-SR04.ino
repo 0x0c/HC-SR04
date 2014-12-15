cd4112757077887c3fd58c2aef98e9f9b0fd51f2
Akira Matsuda <akira.matsuda@me.com>
2014-12-15 23:49:53 +0900

    initial commit

---
+++ HC-SR04.ino
@@ -0,0 +1,25 @@
+#define trigPin 12
+#define echoPin 13
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  int c = 20;
+  distance = (duration/2) * (331.5 + 0.6 * c) * 100 / 1000000;
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Refer to http://www.trollmaker.com/article3/arduino-and-hc-sr04-ultrasonic-sensor
+
+and added small fix.
